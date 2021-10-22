48f28c25026150bdf3ac3aea2f4971a044a0bb9c
SeriousHyena <emackiii@gmail.com>
2021-10-22 06:25:44 -0700

    added overloaded function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <array>
 
 using namespace std;
 
-//declare varibles here
+//declare variables here
 int sumDirect = 0;
 int& sumRef = sumDirect;
+
+//declare arrays here
+array<int, 5> myArray = { 2,4,6,8,10 };
 
 //declare functions here
 int sumArray(int arrayInt, int sumRef);
@@ -12,13 +16,35 @@ int sumArray(int arrayInt, int sumRef);
 int main()
 
 {
-	int sumArray(int arrayInt, int sumRef);
+	int sumArray(int arrayInt, int sumRef);		//call the function
+	
+	int byRef = sumArray(1, sumRef);
+	cout <<"The result by Reference is: " << byRef << endl;
+
+	int byDir = sumArray(1, sumDirect);
+	cout << "The Direct result is: " << byDir << endl;
 	return 0;
 }
 
 //place functions here
 int sumArray(int arrayInt, int sumRef)
 {
-	return 0
+	for (int i = 0; i < myArray.size(); i++)
+	{
+		sumRef += myArray[i];
+	}
+
+	return sumRef;
+}
+
+//this is the overloaded function
+int sumArray(double a, int sumDir)
+{
+	for (int i = 0; i < myArray.size(); i++)
+	{
+		sumDir += myArray[i];
+	}
+
+	return sumDir;
 }
 
