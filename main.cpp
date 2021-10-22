8cc1301d0ed6c1255ca2eff77818514bfca8fe2b
SeriousHyena <emackiii@gmail.com>
2021-10-22 05:51:36 -0700

    initial commit

--- main.cpp
+++ main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+//declare varibles here
+int sumDirect = 0;
+int& sumRef = sumDirect;
+
+//declare functions here
+
 int main()
 {
 
 	return 0;
 }
+
+//place functions here
+
