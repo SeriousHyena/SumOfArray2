01c67810efec41286d4c63a39f46532f5adc1517
SeriousHyena <emackiii@gmail.com>
2021-10-22 05:54:11 -0700

    added function

--- main.cpp
+++ main.cpp
@@ -7,12 +7,18 @@ int sumDirect = 0;
 int& sumRef = sumDirect;
 
 //declare functions here
+int sumArray(int arrayInt, int sumRef);
 
 int main()
-{
 
+{
+	int sumArray(int arrayInt, int sumRef);
 	return 0;
 }
 
 //place functions here
+int sumArray(int arrayInt, int sumRef)
+{
+	return 0
+}
 
